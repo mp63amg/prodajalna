44622bcbe74931aaabaa6ef0fe29085377beb3cd
Mihael <mihael.podplatnik@hotmail.com>
2016-05-08 13:26:28 +0000

    Zaključek dela na I. nalogi

--- eracun.js
+++ eracun.js
@@ -209,13 +209,27 @@ streznik.post('/prijava', function(zahteva, odgovor) {
     	  Phone, Fax, Email, SupportRepId) \
         VALUES (?,?,?,?,?,?,?,?,?,?,?,?)");
       //TODO: add fields and finalize
-      //stmt.run("", "", "", "", "", "", "", "", "", "", "", 3); 
-      //stmt.finalize();
+      stmt.run(polja.FirstName, polja.LastName, polja.Company, polja.Address, polja.City, polja.State, polja.Country, polja.PostalCode, polja.Phone, polja.Fax, polja.Email, 3); 
+      stmt.finalize();
+      
     } catch (err) {
       napaka2 = true;
     }
-  
-    odgovor.end();
+   
+    var uspesnaRegistracija = "Stranka je bila uspešno registrirana.";
+    var prisloJeDoNapake = "Prišlo je do napake pri registraciji nove stranke. Prosim preverite vnešene podatke in poskusite znova.";
+    
+     vrniStranke(function(napaka1, stranke){
+       vrniRacune(function(napaka2, racuni) {
+         
+         if(napaka1 || napaka2){
+             odgovor.render('prijava', {sporocilo: prisloJeDoNapake, seznamStrank: stranke, seznamRacunov: racuni});
+         }else{
+           odgovor.render('prijava', {sporocilo: uspesnaRegistracija, seznamStrank: stranke, seznamRacunov: racuni});
+         }
+       })
+     });  
+    
   });
 })
 
